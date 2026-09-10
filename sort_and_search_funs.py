78505e935403b839c9d7d72338e4176fe8128b83
Heather Ho <heatherho@gmail.com>
2026-09-10 08:24:34 -0700

    add insertion sort

---
+++ sort_and_search_funs.py
@@ -0,0 +1,28 @@
+def insertionSortRecursive(arr,n):
+    # base case
+    if n <= 1:
+        return
+    # Sort first n-1 elements
+    insertionSortRecursive(arr, n-1)
+    # Insert last element at its correct position in sorted array.
+    last = arr[n-1]
+    j = n-2
+    # Move elements of arr[0..i-1], that are greater than key, to one position ahead
+    # of their current position
+    while (j >= 0 and arr[j] > last):
+        arr[j+1] = arr[j]
+        j = j-1
+    arr[j+1] = last
+
+def insertionSortIterative(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        # Move elements of arr[0..i-1], that are greater than key, to one position ahead
+        # of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
